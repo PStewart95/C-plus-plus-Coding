4af5ee4d40240cdbef47d6bf5b999fed81d54680
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-01-17 15:20:24 +0000

    Program to assign values to 5 variables to built-in types

---
+++ BuiltInTypes/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int i = 0;
+    std::cout << i << std::endl;
+    char c = 'G';
+    std::cout << c << std::endl;
+    bool condition = true;
+    std::cout << condition << std::endl;
+    float x = 1.5;
+    std::cout << x << std::endl;
+    double f = 1.560383853;
+    std::cout << f << std::endl;
+    return 0;
+    }
