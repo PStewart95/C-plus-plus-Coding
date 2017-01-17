8979b6b03549a26bcf656018d250eaa67a2f4cb9
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-01-17 16:13:51 +0000

    Modified Built-in Types with read me file

--- BuiltInTypes/main.cpp
+++ BuiltInTypes/main.cpp
@@ -2,14 +2,19 @@
 
 int main() {
     int i = 0;
+    std::cin >> i;
     std::cout << i << std::endl;
     char c = 'G';
+    std::cin >> c;
     std::cout << c << std::endl;
     bool condition = true;
+    std::cin >> condition;
     std::cout << condition << std::endl;
     float x = 1.5;
+    std::cin >> x;
     std::cout << x << std::endl;
     double f = 1.560383853;
+    std::cin >> f;
     std::cout << f << std::endl;
     return 0;
     }
