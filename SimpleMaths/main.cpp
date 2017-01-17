f080dd52ebd160203f78746907f9bf5df6ebed63
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-01-17 16:33:58 +0000

    Simple Math calculator for 5 basic operations

---
+++ SimpleMaths/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+    int a =10;
+    int b =5;
+
+    int x = a+b;
+    std::cout << x << std::endl;
+    int k = a-b;
+    std::cout << k << std::endl;
+    int f = a*b;
+    std::cout << f << std::endl;
+    int z = a/b;
+    std::cout << z << std::endl;
+    int y = a%b;
+    std::cout << y << std::endl;
+    return 0;
+}
