7b061b26ac3cd7c0661eb84c32949263a5744930
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-01-17 16:25:15 +0000

    Division calculator

---
+++ IntegerDivision/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int i =16;
+    std::cin >> i;
+    int x =4;
+    std::cin >> x;
+    float f = float(i)/x;
+    std::cout << f << std::endl;
+    return 0;
+}
