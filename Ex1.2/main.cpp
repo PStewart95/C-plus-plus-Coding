2a393010278272e8af8ef19ad38d73e99d86d71b
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-01-23 16:04:28 +0000

    Sum of 5 floating point numbers

---
+++ Ex1.2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+
+    float a =1;
+    std::cin >> a;
+
+    float b =2;
+    std::cin >> b;
+
+    float c =3;
+    std::cin >> c;
+
+    float d =4;
+    std::cin >> d;
+
+    float e =5;
+    std::cin >> e;
+
+    std::cout << a + b + c+ d + e << std::endl;
+
+    return 0;
+}
