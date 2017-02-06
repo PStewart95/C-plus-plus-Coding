75d023ebbd11af59ac7773b52566692e243884a1
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-02-06 16:24:03 +0000

    Draw shapes with input of sides > 2 and input of side length > 0

--- Ex2.2/main.cpp
+++ Ex2.2/main.cpp
@@ -1,38 +1,32 @@
-/*
- * main.cpp
- *
- *  Created on: 18 Dec 2014
- *      Author: apw253
- */
-
 #include "Window.h"
 
 
 
 int main(int argc, char * argv[]) {
+
+    int nSides;
+    do {
+    std::cin >> nSides;
+    } while(nSides<3);
+
+    int sideLength;
+    do{
+    std::cin>>sideLength;
+    } while(sideLength<1);
+
 // create a new window of size 500 x 500 pixels
 // the top left corner of the window is (0,0)
-	SPA::Window window(500,500,"My Test");
+	SPA::Window window(500, 500, "My Test");
 
-	// Add code here to draw things
-
-	/*
-	 * Here are some of the options
-	 * window.startNewLine(float x, float y) start a new line from location (x,y)
-	 * window.penSetAngle(float angle) to set the initial ang of the pen in degrees
-	 * window.penRotateDegrees(float angle) rotate the pen clockwise in degrees
-	 * window.penForward(float distance) move the pen forward
-	 * window.stopLine() to stop the current line
-*/
-
-	/* uncomment the following lines to see a working example of how to draw lines
-	window.startNewLine(0,250);
-	window.penForward(250);
-	window.penRotateDegrees(90);
-	window.penForward(250);
-	window.stopCurrentLine();
-	 */
-
+	int n=nSides;
+    int a=(250-(sideLength/2));
+    int b= (250-((sideLength/2)*tan((90-(360/(2*n))))*(M_PI/180)));
+    window.startNewLine (a,b);
+    window.penSetAngle (0);
+    for(int i=0;i<n;++i)
+    {window.penForward(sideLength);
+    window.penRotateDegrees(float(360.0)/n);}
+    window.stopCurrentLine();
 	// display the window
 	window.display();
 	// run the FLTK event loop - this allows you to move the window around etc
