6e971ecd5aaeea16c422764a866f0080a3c9abf6
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-02-14 16:14:16 +0000

    Ex2.6 Complete

--- Ex2.6/main.cpp
+++ Ex2.6/main.cpp
@@ -7,34 +7,36 @@
 
 #include "Window.h"
 #include "Colours.h"
+#include <fstream>
 
-
+using namespace std;
 
 int main(int argc, char * argv[]) {
 // create a new window of size 500 x 500 pixels
 // the top left corner of the window is (0,0)
-	SPA::Window window(500,500,"My Test");
+    SPA::Window window(500,500,"My Test");
 
-	// Add code here to draw things
+    ifstream fin("G:/points2.dat");
+    int npoints;
+    fin >> npoints;
+    cout << npoints << endl;
 
-	/*
-	 * Here are some of the options
-	 * window.startNewLine(float x, float y) start a new line from location (x,y)
-	 * window.setAngle(float angle) to set the initial ang of the pen in degrees
-	 * window.penRotateDegrees(float angle) rotate the pen in degrees
-	 * window.penForward(float distance) move the pen forward
-	 * window.stopCurrentLine() to stop the current line
-	 * window.addPointfloat x, float y) add a point to the current line
-	 * window.addPoint(Point p) add a point to the current line
-	 * window.setLineColor(FL_Color c) set the colour for the next line
-*/
+    int a;
+    int b;
+    for(int i=0; i<=npoints; i++) {
+        fin >> a >> b;
 
-	// display the window
-	window.show(argc,argv);
-	// run the FLTK event loop - this allows you to move the window around etc
-	// and ultimately set up click events.
-	// program will end when you close the window
-	return Fl::run();
+    cout<< a << " "<< b << " " << fin.good() << endl;
+
+    window.addPoint(a,b);
+
+    if (!fin.good() ) break;
+
+    }
+
+    window.stopCurrentLine();
+
+    window.show(argc,argv);
+
+return Fl::run();
 }
-
-
