547bc514c4b86f6185343cc041e7ce349c34623f
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-02-16 20:19:44 +0000

    Ex2.8 Complete two rotating squares one red and one black

--- Ex2.8/main.cpp
+++ Ex2.8/main.cpp
@@ -13,41 +13,46 @@ using namespace SPA;
 
 
 int main(int argc, char * argv[]) {
-// create a new window of size 500 x 500 pixels
-// the top left corner of the window is (0,0)
+
 	SPA::Window window(500,500,"My Test",24);
 	Fl::visual(FL_DOUBLE | FL_INDEX);
 
-	// Add code here to draw things
+
 	Shape * shape = new Shape(250,250,0);
-	shape->setRotationSpeed(2); // initial rotation speed in degrees
-	// make a new line
+	shape->setRotationSpeed(2);
+
 	Line * line = new Line();
-	// allowed colours are specified in the enum in Colours.hpp
+
 	line->setColor(getColor(BLACK));
-	// add a point to the line
-	// these points are all relative to the centre of the Shape
+
 	line->addPoint(Point(-100,-100));
 	line->addPoint(Point(100,-100));
 	line->addPoint(Point(100,100));
 	line->addPoint(Point(-100,100));
 	line->addPoint(Point(-100,-100));
-	// add this line to the shape
+
+    Shape * square = new Shape(250,250,45);
+    shape->setRotationSpeed(-2);
+
+    Line * line1 = new Line();
+
+    line1->setColor(getColor(RED));
+
+    line1->addPoint(Point(0,0));
+    line1->addPoint(Point(100,0));
+    line1->addPoint(Point(100,100));
+    line1->addPoint(Point(0,100));
+    line1->addPoint(Point(0,0));
+    square->addLine(line1);
+    window.addShape(square);
+
 	shape->addLine(line);
-	// add the shape to the window
+
 	window.addShape(shape);
-	// notice the use of . notation for the window
-	// and contrast with the use of -> notation for the shape and line
+    ////////
 
-	// add your code here!
-
-
-	////////
-	// display the window
 	window.show(argc,argv);
-	// run the FLTK event loop - this allows you to move the window around etc
-	// and ultimately set up click events.
-	// program will end when you close the window
+
 	return Fl::run();
 }
 
