86b152c8d63a23b055e82604fc50e8e0807d1223
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-02-16 17:29:29 +0000

    Ex2.7 Part A Commit 2

---
+++ Ex2.7/functions.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ap14058 on 16/02/2017.
+//
+
+#include "functions.h"
---
+++ Ex2.7/functions.h
@@ -0,0 +1,12 @@
+//
+// Created by ap14058 on 16/02/2017.
+//
+
+#ifndef EX2_7_FUNCTIONS_H
+#define EX2_7_FUNCTIONS_H
+#include <vector>
+#include <fstream>
+
+void readPointsFromStream(int n, std::vector<float> & xvec, std::vector <float> & yvec, std::ifstream & f);
+
+#endif //EX2_7_FUNCTIONS_H
