cd539e0a058a6c414b067f48dfcd8f6028144666
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-02-16 17:58:56 +0000

    Ex2.7 Part A Complete

--- Ex2.7/functions.cpp
+++ Ex2.7/functions.cpp
@@ -2,4 +2,10 @@
 // Created by ap14058 on 16/02/2017.
 //
 
+#include <iostream>
 #include "functions.h"
+using namespace std;
+void readPointsFromStream(int n, std::vector<float> & xvec, std::vector <float> & yvec, std::ifstream & f ){
+    cout<< n << endl;
+
+}
--- Ex2.7/main.cpp
+++ Ex2.7/main.cpp
@@ -9,6 +9,7 @@
 #include "Colours.h"
 #include "functions.h"
 #include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -16,11 +17,20 @@ int main(int argc, char * argv[]) {
 
 	SPA::Window window(500,500,"My Test");
 
-    ifstream file("G:/points3.dat");
+    ifstream myInputFile;
+    myInputFile.open("G:/points3.dat");
 
-    file.open;
+    int n;
+    myInputFile>>n;
 
-	window.show(argc,argv);
+    vector<float> xvec;
+    vector<float> yvec;
+
+    readPointsFromStream( n, xvec, yvec, myInputFile);
+
+    for (int i=0; i=n; ++i)
+
+    window.show(argc,argv);
 
 	return Fl::run();
 }
