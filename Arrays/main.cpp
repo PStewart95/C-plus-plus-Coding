ea18bdb2d6cf6573495a4560b5b62b1f9166e8a2
ap14058 <p.stewart@se14.qmul.ac.uk>
2017-03-27 13:07:10 +0100

    Arrays Ex1

---
+++ Arrays/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int array[10];
+
+    for(int i=1; i<11; ++i)
+    array[i]=i;
+
+    for(int i=1; i<11; ++i)
+    { std::cout << array[i] << std::endl; }
+
+    return 0;
+}
